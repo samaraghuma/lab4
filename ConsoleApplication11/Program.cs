31b8cb52a0fb7c573ff374ca7b4100f8a6b7e2b8
raghuma sama <raghuma sama>
2016-10-26 20:08:58 -0400

    first commit

---
+++ ConsoleApplication11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void factorial()
+        {
+
+            Console.WriteLine("enter the integer 1 to 10");
+            int usernum = Convert.ToInt32(Console.ReadLine());
+            for (int i = usernum - 1; i >= 1; i--)
+            {
+                usernum = usernum * i;
+
+
+            }
+            Console.WriteLine(usernum);
+
+        }
+        static void Main(string[] args)
+        {
+            factorial();
+               
+        }
+    }
+
+}
